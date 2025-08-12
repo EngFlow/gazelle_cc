bbfec4b46b862ea415dfd27c05bea136088c3525
Mateusz Olczyk <molczyk@virtuslab.com>
2025-08-12 13:33:52 +0200

    Enforce no warnings when calling 'bazel run :gazelle'
    Signed-off-by: Mateusz Olczyk <molczyk@virtuslab.com>

--- .github/workflows/test_e2e.sh
+++ .github/workflows/test_e2e.sh
@@ -5,20 +5,39 @@ set -o errexit -o nounset -o pipefail
 scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 rootDir=$(realpath "$scriptDir/../..")
 
+function runBazelCommandTreatingWarningsAsErrors() {
+  local CMD="$1"
+  shift
+  local OUTPUT_BASE_DIR=$(bazel info output_base)
+  local BAZEL_COMMAND_LOG_FILE="$OUTPUT_BASE_DIR/command.log"
+
+  # Disable colors and control characters to make grep work properly
+  bazel "$CMD" --color=no --curses=no "$@"
+
+  # Expect no warnings in the command output
+  local COLLECTED_WARNINGS=$(grep "^WARNING" "$BAZEL_COMMAND_LOG_FILE" || true)
+  if [[ -n "$COLLECTED_WARNINGS" ]]; then
+    echo >&2
+    echo "Unexpected warnings found:" >&2
+    echo "$COLLECTED_WARNINGS" >&2
+    exit 1
+  fi
+}
+
 function testExampleBzlMod() {
   echo "Test example/bzlmod"
   cd "$rootDir/example/bzlmod"
-  
+
   # Ensure previous BUILD files are removed
   rm -f mylib/BUILD.bazel proto/BUILD.bazel
-  
+
   # Run gazelle to generate BUILD files
-  bazel run :gazelle
-  
+  runBazelCommandTreatingWarningsAsErrors run :gazelle
+
   # Verify that BUILD files were generated
   test -f mylib/BUILD.bazel
   test -f proto/BUILD.bazel
-  
+
   bazel build //...
   bazel test --test_output=errors //...
   bazel run //proto:example 
@@ -27,17 +46,17 @@ function testExampleBzlMod() {
 function testExampleWorkspace() {
   echo "Test example/workspace"
   cd "$rootDir/example/workspace"
-  
+
   # Ensure previous BUILD files are removed
   rm -f mylib/BUILD.bazel app/BUILD.bazel
-  
+
   # Run gazelle to generate BUILD files
-  bazel run :gazelle
-  
+  runBazelCommandTreatingWarningsAsErrors run :gazelle
+
   # Verify that BUILD files were generated
   test -f mylib/BUILD.bazel
   test -f app/BUILD.bazel
-  
+
   bazel build //...
   bazel test --test_output=errors //...
   bazel run //app:main 
