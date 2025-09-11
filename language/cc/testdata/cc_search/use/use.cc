bf0f366f77e8419e707f50be36e44e3c92e07856
Mateusz Olczyk <molczyk@virtuslab.com>
2025-09-11 16:48:57 +0200

    Fix cc_search test
    Signed-off-by: Mateusz Olczyk <molczyk@virtuslab.com>

--- language/cc/testdata/cc_search/use/use.cc
+++ language/cc/testdata/cc_search/use/use.cc
@@ -2,7 +2,7 @@
 #include "self/self.h"
 #include "stripped_prefix_abs.h"
 #include "stripped_prefix_rel.h"
-#include "extra/stripped_include_prefix_and_prefix.h"
+#include "extra/stripped_prefix_and_prefix.h"
 
 void use() {
 	prefix();
