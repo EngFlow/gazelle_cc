3e4c05223900274ea067f0c5019e2be47cf8b00b
Mateusz Olczyk <molczyk@virtuslab.com>
2025-10-10 11:08:46 +0200

    Implement new language/internal/cc/lexer package (#111)
    * Implement new language/internal/cc/lexer package
    * Drop bufio.Scanner usage and prepare a logic based on regexes
    
    ---------
    
    Signed-off-by: Mateusz Olczyk <molczyk@virtuslab.com>

---
+++ language/internal/cc/lexer/lexer.go
@@ -0,0 +1,97 @@
+// Copyright 2025 EngFlow Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package lexer provides a lexical analyzer for the C/C++ source code. It breaks the input into a sequence of tokens,
+// which can then be processed by a parser.
+//
+// Lexer classifies tokens into several types (for e.g., easier filtering comments or whitespace) and tracks their
+// location in the source code (for accurate error reporting).
+package lexer
+
+import "regexp"
+
+// Represents a way of matching a specific token type using a regular expression.
+type matcher struct {
+	matchedType TokenType
+	matchingRe  *regexp.Regexp
+}
+
+// Matching logic for all token types apart from TokenType_Word which is the default fallback type when no other
+// matchers apply.
+var matchers = []matcher{
+	{matchedType: TokenType_Symbol, matchingRe: regexp.MustCompile(`(?:[!=<>]=?|&&?|\|\|?|[(){}\[\],;])`)},
+	{matchedType: TokenType_PreprocessorDirective, matchingRe: regexp.MustCompile(`#[\t\v\f\r ]*\w+`)},
+	{matchedType: TokenType_Newline, matchingRe: regexp.MustCompile(`\n`)},
+	{matchedType: TokenType_Whitespace, matchingRe: regexp.MustCompile(`[\t\v\f\r ]+`)},
+	{matchedType: TokenType_ContinueLine, matchingRe: regexp.MustCompile(`\\[\t\v\f\r ]*\n`)},
+	{matchedType: TokenType_SingleLineComment, matchingRe: regexp.MustCompile(`//[^\n]*`)},
+	{matchedType: TokenType_MultiLineComment, matchingRe: regexp.MustCompile(`(?s)/\*.*?\*/`)},
+}
+
+type Lexer struct {
+	dataLeft []byte
+	cursor   Cursor
+}
+
+func NewLexer(sourceCode []byte) *Lexer {
+	return &Lexer{dataLeft: sourceCode, cursor: CursorInit}
+}
+
+// Update the lexer state accordingly to the extracted token content.
+func (lx *Lexer) consume(content string) {
+	lx.dataLeft = lx.dataLeft[len(content):]
+	lx.cursor = lx.cursor.AdvancedBy(content)
+}
+
+// Return the next token extracted from the beginning of the input data left to process. If no more tokens are left,
+// returns TokenEmpty.
+func (lx *Lexer) NextToken() Token {
+	if len(lx.dataLeft) == 0 {
+		return TokenEmpty
+	}
+
+	// Try each matcher looking for the earliest match.
+	tokenBegin := len(lx.dataLeft)
+	tokenEnd := len(lx.dataLeft)
+	tokenType := TokenType_Word
+	for _, m := range matchers {
+		if match := m.matchingRe.FindIndex(lx.dataLeft); match != nil && match[0] < tokenBegin {
+			tokenBegin = match[0]
+			tokenEnd = match[1]
+			tokenType = m.matchedType
+		}
+	}
+
+	var result Token
+	if tokenBegin == 0 {
+		// Something matched at the beginning, so return that token.
+		result = Token{Type: tokenType, Location: lx.cursor, Content: string(lx.dataLeft[tokenBegin:tokenEnd])}
+	} else {
+		// If nothing matched at the beginning of the text, return a word token up to the next match. If nothing matched
+		// anywhere, use the rest of the text.
+		result = Token{Type: TokenType_Word, Location: lx.cursor, Content: string(lx.dataLeft[:tokenBegin])}
+	}
+
+	lx.consume(result.Content)
+	return result
+}
+
+// Return all tokens extracted from the input data.
+func (lx *Lexer) Tokenize() []Token {
+	var tokens []Token
+	for len(lx.dataLeft) > 0 {
+		tokens = append(tokens, lx.NextToken())
+	}
+	return tokens
+}
---
+++ language/internal/cc/lexer/token.go
@@ -0,0 +1,51 @@
+// Copyright 2025 EngFlow Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package lexer
+
+type TokenType int
+
+const (
+	// Every complete token that is not one of the other types, e.g. identifiers, keywords.
+	TokenType_Word TokenType = iota
+
+	// One of predefined fixed-size sequences of characters, e.g. '(', '==', ';', '&&'.
+	TokenType_Symbol
+
+	// Preprocessor directive, a hash '#' followed by the directive name (with optional whitespace characters between).
+	TokenType_PreprocessorDirective
+
+	// Single newline character '\n'. Newlines require special handling because they mark the end of a preprocessor directive.
+	TokenType_Newline
+
+	// One or more whitespace characters, other than newlines.
+	TokenType_Whitespace
+
+	// Line continuation sequence, a backslash '\' followed by a newline character '\n' (with optional whitespace characters between).
+	TokenType_ContinueLine
+
+	// Single-line comment, starting with // and ending at the end of the line.
+	TokenType_SingleLineComment
+
+	// Multi-line comment, starting with /* and ending with */.
+	TokenType_MultiLineComment
+)
+
+type Token struct {
+	Type     TokenType
+	Location Cursor
+	Content  string
+}
+
+var TokenEmpty = Token{}
