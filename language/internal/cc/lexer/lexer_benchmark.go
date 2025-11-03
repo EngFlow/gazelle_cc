325de5b34bada6d2f6818a40cea9ee942ae5c26e
Mateusz Olczyk <molczyk@virtuslab.com>
2025-11-03 15:15:49 +0100

    Prepare a benchmark for Lexer
    Signed-off-by: Mateusz Olczyk <molczyk@virtuslab.com>

---
+++ language/internal/cc/lexer/lexer_benchmark.go
@@ -0,0 +1,49 @@
+// Copyright 2025 EngFlow Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package lexer
+
+import (
+	"bytes"
+	"slices"
+	"testing"
+)
+
+const helloWorldInput = `
+#include <iostream>
+
+int main(int argc, char **argv) {
+    std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
+`
+
+func benchmark(b *testing.B, input []byte) {
+	for i := 0; i < b.N; i++ {
+		lexer := NewLexer(input)
+		_ = slices.Collect(lexer.AllTokens())
+	}
+}
+
+func BenchmarkRepeatedToken(b *testing.B) {
+	benchmark(b, bytes.Repeat([]byte(";"), 1000))
+}
+
+func BenchmarkHelloWorld(b *testing.B) {
+	benchmark(b, []byte(helloWorldInput))
+}
+
+func BenchmarkRepeatedHelloWorld(b *testing.B) {
+	benchmark(b, bytes.Repeat([]byte(helloWorldInput), 100))
+}
